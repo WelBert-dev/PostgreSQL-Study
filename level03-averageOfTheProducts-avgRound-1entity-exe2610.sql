87a5d06c6c0d7041848289e6ec1ee3e2e7f63ac6
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-12 14:50:26 -0300

    add the challenge link

--- level03-averageOfTheProducts-avgRound-1entity-exe2610.sql
+++ level03-averageOfTheProducts-avgRound-1entity-exe2610.sql
@@ -1,8 +1,9 @@
--- In the company that you work is being done a survey on the values of the products that are marketed.
+-- Challenge Link: https://www.beecrowd.com.br/judge/en/problems/view/2610
 
--- To help the industry that is doing this survey you should calculate and display the average value of the price of the products.
-
--- OBS: Show the value with two numbers after the period.
+-- Challenge description:
+    -- In the company that you work is being done a survey on the values of the products that are marketed.
+    -- To help the industry that is doing this survey you should calculate and display the average value of the price of the products.
+    -- OBS: Show the value with two numbers after the period.
 
 -- Schema:
     -- [products]
